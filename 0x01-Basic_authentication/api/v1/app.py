47a8b9f0fa7da97380fefe90f720d8997c32adbb
amch-med23 <skyied.med2023@gmail.com>
2024-05-20 14:53:31 +0000

    adding the api/ folder

---
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+
+# check the AUTH_TYPE
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+# befour the request,the exclude list.
+
+
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ this functionn handles unauthorized requests"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ this to handle the forbidden requests """
+    return jsonify({"error": "Forbidden"}), 403
