48dbb2c60b517fa08dac29e978214b4498a2224b
amch-med23 <skyied.med2023@gmail.com>
2024-05-21 10:24:51 +0000

    fixing the extract_base64_authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,10 +15,12 @@ class BasicAuth(Auth):
         """ a method to extract the base64 string from the header """
         if authorization_header is None:
             return None
+        if not isinstance(authorization_header, str):
+            return None
         if not authorization_header.startswith('Basic '):
             return None
 
-        token = authorization_header.split(' ')[:-1]
+        token = authorization_header.split(' ')[1:]
         return token
 
     def decode_base64_authorization_header(self,
