55d61819b5477bd43938f205a0989ed6b13bb9d2
amch-med23 <skyied.med2023@gmail.com>
2024-05-22 15:14:27 +0000

    creting the project folder based ona copy of the 0x01-Basic_authentication project folder

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""" the basic authentication model """
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ the auth class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ this is to check the authentication requirements """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_paths[:-1]):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ the header authorization method """
+        if request is None:
+            return None
+
+        # retreaives the header from the request
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return None
+
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ the current authorized (authenticated) user """
+        return None
